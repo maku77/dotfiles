daed087bbad9add87958b70b7499f11815df023f
maku77 <makkuma77@gmail.com>
2015-12-01 22:11:02 +0900

    Add setup.bat for modifying %HOME%/_vimrc.

---
+++ vim/setup.bat
@@ -0,0 +1,12 @@
+@echo off
+if not defined HOME (
+  echo 環境変数 HOME が定義されていません
+  echo 環境変数 HOME に _vimrc を配置するためのディレクトリパスを設定してください
+  exit /b
+)
+
+set MY_DIR=%~dp0
+@echo on
+echo source %MY_DIR%vimrc >> %HOME%\_vimrc
+echo source %MY_DIR%gvimrc >> %HOME%\_gvimrc
+
