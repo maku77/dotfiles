adb527f34ec28947427238c1eb0db2223e699e60
maku77 <makkuma77@gmail.com>
2024-03-27 03:30:06 +0900

    Add script to create sym links

---
+++ create-symlinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# このシェルスクリプトが格納されているディレクトリの絶対パス
+self_dir=$(cd $(dirname $0); pwd)
+
+###
+### Alacritty 設定ファイルのシンボリックリンクを作成
+###
+mkdir -p ~/.config/alacritty
+alacritty_link=~/.config/alacritty/alacritty.toml
+alacritty_target="$self_dir/alacritty/alacritty.toml"
+
+echo "Create Alacritty config: $alacritty_link"
+ln -s "$alacritty_target" "$alacritty_link"
+
